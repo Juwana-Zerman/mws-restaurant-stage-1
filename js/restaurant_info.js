f18a9eb99a365db0503ca6463e2e83147f85518e
Juwana <juwanazerman@gmail.com>
2018-09-01 23:05:34 -0400

    Update Code

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,40 +1,35 @@
 let restaurant;
 var newMap;
-
 /**
  * Initialize map as soon as the page is loaded.
  */
-document.addEventListener('DOMContentLoaded', (event) => {  
-  initMap();
+document.addEventListener('DOMContentLoaded', (event) => {
+	initMap();
 });
-
 /**
  * Initialize leaflet map
  */
 initMap = () => {
-  fetchRestaurantFromURL((error, restaurant) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {      
-      self.newMap = L.map(document.getElementById('map'), {
-        center: [restaurant.latlng.lat, restaurant.latlng.lng],
-        zoom: 16,
-        scrollWheelZoom: false
-      });
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1Ijoianplcm1hbiIsImEiOiJjamt2ZnY2dXMwcTV3M3dtdHFsY3VlaDhoIn0.9Foo09oDgGM6BQYlTssuBA',
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'    
-      }).addTo(newMap);
-      fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
-    }
-  });
-}  
- 
+	fetchRestaurantFromURL((error, restaurant) => {
+		if (error) { // Got an error!
+			console.error(error);
+		} else {
+			self.newMap = L.map(document.getElementById('map'), {
+				center: [restaurant.latlng.lat, restaurant.latlng.lng],
+				zoom: 16,
+				scrollWheelZoom: false
+			});
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+				mapboxToken: 'pk.eyJ1Ijoianplcm1hbiIsImEiOiJjamt2ZnY2dXMwcTV3M3dtdHFsY3VlaDhoIn0.9Foo09oDgGM6BQYlTssuBA',
+				maxZoom: 18,
+				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+				id: 'mapbox.streets'
+			}).addTo(newMap);
+			fillBreadcrumb();
+			DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
+		}
+	});
+}
 /*
  window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
@@ -51,185 +46,124 @@ initMap = () => {
     }
   });
 } */
-
 /**
  * Get current restaurant from page URL.
  */
 fetchRestaurantFromURL = (callback) => {
-  if (self.restaurant) { // restaurant already fetched!
-    callback(null, self.restaurant)
-    return;
-  }
-  const id = getParameterByName('id');
-  if (!id) { // no id found in URL
-    error = 'No restaurant id in URL'
-    callback(error, null);
-  } else {
-    DBHelper.fetchRestaurantById(id, (error, restaurant) => {
-      self.restaurant = restaurant;
-      if (!restaurant) {
-        console.error(error);
-        return;
-      }
-      fillRestaurantHTML();
-      callback(null, restaurant)
-    });
-  }
+	if (self.restaurant) { // restaurant already fetched!
+		callback(null, self.restaurant)
+		return;
+	}
+	const id = getParameterByName('id');
+	if (!id) { // no id found in URL
+		error = 'No restaurant id in URL'
+		callback(error, null);
+	} else {
+		DBHelper.fetchRestaurantById(id, (error, restaurant) => {
+			self.restaurant = restaurant;
+			if (!restaurant) {
+				console.error(error);
+				return;
+			}
+			fillRestaurantHTML();
+			callback(null, restaurant)
+		});
+	}
 }
-
 /**
  * Create restaurant HTML and add it to the webpage
  */
 fillRestaurantHTML = (restaurant = self.restaurant) => {
-  const name = document.getElementById('restaurant-name');
-  name.innerHTML = restaurant.name;
-
-  const address = document.getElementById('restaurant-address');
-  address.innerHTML = restaurant.address;
-
-  const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
-  const cuisine = document.getElementById('restaurant-cuisine');
-  cuisine.innerHTML = restaurant.cuisine_type;
-
-  // fill operating hours
-  if (restaurant.operating_hours) {
-    fillRestaurantHoursHTML();
-  }
-  // fill reviews
-  fillReviewsHTML();
+	const name = document.getElementById('restaurant-name');
+	name.innerHTML = restaurant.name;
+	const address = document.getElementById('restaurant-address');
+	address.innerHTML = restaurant.address;
+	const image = document.getElementById('restaurant-img');
+	image.className = 'restaurant-img'
+	image.src = DBHelper.imageUrlForRestaurant(restaurant);
+	const cuisine = document.getElementById('restaurant-cuisine');
+	cuisine.innerHTML = restaurant.cuisine_type;
+	// fill operating hours
+	if (restaurant.operating_hours) {
+		fillRestaurantHoursHTML();
+	}
+	// fill reviews
+	fillReviewsHTML();
 }
-
 /**
  * Create restaurant operating hours HTML table and add it to the webpage.
  */
 fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
-  const hours = document.getElementById('restaurant-hours');
-  for (let key in operatingHours) {
-    const row = document.createElement('tr');
-/*
-    //Create Attribute for Tabindex on row
-    let label_tabindex= document.createAttribute("tabindex");
-    label_tabindex.value = 0;
-    //Set the attribute to the row
-    row.setAttributeNode(label_tabindex);
-
-    //Aria labelled by
-    let label_attribute = document.createAttribute("aria-labelledby");
-    label_attribute.value = key + "_label";
-    row.setAttributeNode(label_attribute);*/
-
-    const day = document.createElement('td');
-    day.innerHTML = key;
-    row.appendChild(day);
-
-    const time = document.createElement('td');
-    time.innerHTML = operatingHours[key];
-    row.appendChild(time);
-
-    hours.appendChild(row);
-/*
-    //Aria label for row that speaks day & hours
-    let aria_label = document.createElement('label');
-    aria_label.id = key + "_label";
-    aria_label.className = "aria-label";
-    aria_label.innerHTML = key + operatingHours[key];*/
-  }
+	const hours = document.getElementById('restaurant-hours');
+	for (let key in operatingHours) {
+		const row = document.createElement('tr');
+		const day = document.createElement('td');
+		day.innerHTML = key;
+		row.appendChild(day);
+		const time = document.createElement('td');
+		time.innerHTML = operatingHours[key];
+		row.appendChild(time);
+		hours.appendChild(row);
+	}
 }
-
 /**
  * Create all reviews HTML and add them to the webpage.
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
-  const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
-  title.innerHTML = 'Reviews';
-  container.appendChild(title);
-
-  if (!reviews) {
-    const noReviews = document.createElement('p');
-    noReviews.innerHTML = 'No reviews yet!';
-    container.appendChild(noReviews);
-    return;
-  }
-  const ul = document.getElementById('reviews-list');
-  reviews.forEach(review => {
-    ul.appendChild(createReviewHTML(review));
-  });
-  container.appendChild(ul);
+	const container = document.getElementById('reviews-container');
+	const title = document.createElement('h2');
+	title.innerHTML = 'Reviews';
+	container.appendChild(title);
+	if (!reviews) {
+		const noReviews = document.createElement('p');
+		noReviews.innerHTML = 'No reviews yet!';
+		container.appendChild(noReviews);
+		return;
+	}
+	const ul = document.getElementById('reviews-list');
+	reviews.forEach(review => {
+		ul.appendChild(createReviewHTML(review));
+	});
+	container.appendChild(ul);
 }
-
 /**
  * Create review HTML and add it to the webpage.
  */
 createReviewHTML = (review) => {
-  const li = document.createElement('li');
-  const name = document.createElement('p');
-  name.innerHTML = review.name;
-  li.appendChild(name);
-
-  const date = document.createElement('p');
-  date.innerHTML = review.date;
-  li.appendChild(date);
-
-  const rating = document.createElement('p');
-  rating.innerHTML = `Rating: ${review.rating}`;
-  li.appendChild(rating);
-
-  const comments = document.createElement('p');
-  comments.innerHTML = review.comments;
-  
-  comments.setAttribute('tabindex', '0');
-  li.appendChild(comments);
-  //li.setAttribute('tabindex', '8');
-  //li.setAttribute('aria-label', 'Reviews');
-/*
-  // Add Tab Index for the List Element
-  var label_tabindex = document.createAttribute("tabindex");       
-  label_tabindex.value = 0;
-  // Set the attribute to the row
-  li.setAttributeNode(label_tabindex);
-
-  // Add Aria LabelledBy Attribute for Review
-  var label_attribute = document.createAttribute("aria-labelledby");    
-  label_attribute.value = review_id + "_label";                         
-  li.setAttributeNode(label_attribute); 
-
-  // Add Aria Label for Single Review
-  var aria_label = document.createElement('label');
-  aria_label.id = review_id + "_label";
-  aria_label.className = "aria-label";
-  aria_label.innerHTML = "Rating " + review.rating + " stars. Date " + review.date + ". Reviewed By " + review.name + ". Comments: " + review.comments;
-
-  li.appendChild(aria_label);*/
-
-  return li;
+	const li = document.createElement('li');
+	const name = document.createElement('p');
+	name.innerHTML = review.name;
+	li.appendChild(name);
+	const date = document.createElement('p');
+	date.innerHTML = review.date;
+	li.appendChild(date);
+	const rating = document.createElement('p');
+	rating.innerHTML = `Rating: ${review.rating}`;
+	li.appendChild(rating);
+	const comments = document.createElement('p');
+	comments.innerHTML = review.comments;
+	comments.setAttribute('tabindex', '0');
+	li.appendChild(comments);
+	return li;
 }
-
 /**
  * Add restaurant name to the breadcrumb navigation menu
  */
-fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
-  const li = document.createElement('li');
-  li.innerHTML = restaurant.name;
-  breadcrumb.appendChild(li);
+fillBreadcrumb = (restaurant = self.restaurant) => {
+	const breadcrumb = document.getElementById('breadcrumb');
+	const li = document.createElement('li');
+	li.innerHTML = restaurant.name;
+	breadcrumb.appendChild(li);
 }
-
 /**
  * Get a parameter by name from page URL.
  */
 getParameterByName = (name, url) => {
-  if (!url)
-    url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
-  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
-    results = regex.exec(url);
-  if (!results)
-    return null;
-  if (!results[2])
-    return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, '\\$&');
+	const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
--- js/sw_register.js
+++ js/sw_register.js
@@ -1,14 +1,11 @@
 // sw start
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function(reg) {
-
-      if(reg.active) {
-        console.log('Service worker is active');
-      }
-      
-
-    }).catch(function() {
-      console.log('Service worker registration failed');
-    });
-  }
-  // sw end
+	navigator.serviceWorker.register('./sw.js').then(function(reg) {
+		if (reg.active) {
+			console.log('Service worker is active');
+		}
+	}).catch(function() {
+		console.log('Service worker registration failed');
+	});
+}
+// sw end

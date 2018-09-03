6bc7dca17b6b7efa5726d96af99365971e85769c
Juwana <juwanazerman@gmail.com>
2018-09-03 13:05:42 -0400

    Update img alt for accessibility

--- js/main.js
+++ js/main.js
@@ -143,6 +143,7 @@ createRestaurantHTML = (restaurant) => {
 	const image = document.createElement('img');
 	image.className = 'restaurant-img';
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
+	image.alt = `${restaurant.name} Restaurant`;
 	li.append(image);
 	const name = document.createElement('h1');
 	name.innerHTML = restaurant.name;

1f2b72ce4b08f5325251ff714d0a211ff050de46
Juwana Zerman <38802201+Juwana-Zerman@users.noreply.github.com>
2021-11-09 01:26:49 -0500

    Update mapbox styles link

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ initMap = () => {
 		zoom: 12,
 		scrollWheelZoom: false
 	});
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+	L.tileLayer('https://api.mapbox.com/styles/v1/jzerman?access_token={mapboxToken}', {
 		mapboxToken: 'pk.eyJ1Ijoianplcm1hbiIsImEiOiJjamt5cnB5emEwbWVxM3ZvZWV5NG05MDh1In0.JQl1U5MuY5w5T1SIsYkiPg',
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
